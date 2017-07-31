4ff3df2963a900f6a97aef87822b0a7582bb3ae8
Mikle Gorbunov <mihailgorbunov74@gmail.com>
2017-07-30 17:42:35 -0700

    all bonus missons

--- src/org/launchcode/techjobs/console/JobData.java
+++ src/org/launchcode/techjobs/console/JobData.java
@@ -7,10 +7,7 @@ import org.apache.commons.csv.CSVRecord;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by LaunchCode
@@ -43,7 +40,7 @@ public class JobData {
                 values.add(aValue);
             }
         }
-
+        Collections.sort(values);
         return values;
     }
 
@@ -51,8 +48,12 @@ public class JobData {
 
         // load data, if not already loaded
         loadData();
-
-        return allJobs;
+        ArrayList<HashMap<String, String>> allJobsClone = new ArrayList<>();
+        for (HashMap<String, String> row : allJobs)
+        {
+            allJobsClone.add(row);
+        }
+        return allJobsClone;
     }
 
     /**
